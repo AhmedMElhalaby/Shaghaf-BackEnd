07a9e664962be3b0a541468c72ecf7da2afaf9e7
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2021-04-01 21:07:18 +0300

    Update show.blade.php

--- resources/views/Admin/UserManagement/Provider/show.blade.php
+++ resources/views/Admin/UserManagement/Provider/show.blade.php
@@ -76,6 +76,8 @@
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div class="row">
                         <div class="col-md-4">
                             <div class="card">
                                 <div class="card-header text-center" style="padding: 5px" data-background-color="{{ config('app.color') }}">
