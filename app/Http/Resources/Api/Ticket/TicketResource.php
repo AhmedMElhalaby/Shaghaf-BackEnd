1281d14f2d8a2273b56e35995c4cd27af084209e
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2021-05-23 20:07:53 +0300

    Update TicketResource.php

--- app/Http/Resources/Api/Ticket/TicketResource.php
+++ app/Http/Resources/Api/Ticket/TicketResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Api\Ticket;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TicketResource extends JsonResource
@@ -15,6 +16,7 @@ class TicketResource extends JsonResource
         $Objects['attachment'] = $this->getAttachment();
         $Objects['status'] = $this->getStatus();
         $Objects['status_str'] = __('crud.Ticket.Statuses.'.$this->getStatus());
+        $Objects['created_at'] = Carbon::parse($this->created_at)->diffForHumans();
         $Objects['TicketResponses'] = TicketResponseResource::collection($this->ticket_responses);
         return $Objects;
     }
